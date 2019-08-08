4ea1af2a62f8af4cc63cc25011ecade40937370d
iketheadore <2589926+iketheadore@users.noreply.github.com>
2019-08-08 18:54:42 +1000

    Force redirect http to https

--- linode/StackScripts.sh
+++ linode/StackScripts.sh
@@ -3,10 +3,10 @@
 #<UDF name="hostname" label="The host name for the new Linode.">
 # HOSTNAME=
 #
-#<UDF name="fqdn" label="The new Linode's Full Qualified Domain Name. Bind server ip to it once the server is up.">
+#<UDF name="fqdn" label="The new Linode's Full Qualified Domain Name, bind the server ip to it once the server is up.">
 # FQDN=
 #
-#<UDF name="email" label="The email address for receiving notifications from letsEncrypt">
+#<UDF name="email" label="The email address for receiving messages from letsEncrypt">
 # EMAIL=
 #
 
@@ -30,10 +30,9 @@ apt-get install python-certbot-nginx -y
 # install nginx
 apt-get install nginx -y
 systemctl enable nginx
-
 cat << EOF>/etc/nginx/conf.d/$FQDN.conf
 server {
-
+  listen          80;
   server_name $FQDN;
 
   location / {
@@ -44,7 +43,32 @@ server {
 EOF
 
 # enable https with letsEncrypt
-certbot run --nginx -n --agree-tos -d $FQDN --email $EMAIL
+certbot certonly --nginx --agree-tos -n -d $FQDN --email $EMAIL
+
+# Update nginx config to enable https
+cat << EOF>/etc/nginx/conf.d/$FQDN.conf
+server {
+
+    server_name $FQDN;
+
+    location / {
+        proxy_pass http://localhost:6420;
+        proxy_set_header X-Real-IP \$remote_addr;
+    }
+
+    listen 443 ssl; # managed by Certbot
+    ssl_certificate /etc/letsencrypt/live/$FQDN/fullchain.pem; # managed by Certbot
+    ssl_certificate_key /etc/letsencrypt/live/$FQDN/privkey.pem; # managed by Certbot
+    include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+}
+
+server {
+  listen          80;
+  server_name     $FQDN;
+  return          301 https://\$server_name\$request_uri;
+}
+EOF
 
 # restart nginx service
 systemctl restart nginx
